880d5c61cf9d719d88f0739e6458005ca75ceb8e
Dillon Parchment <dillonparchmentkool@gmail.com>
2021-03-28 13:37:13 -0500

    useState, custom hooks, update ESLint, bring in petfinder client

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Pet from './Pet';
-const App = () => {
-  //   return React.createElement('div', {}, [
-  //     React.createElement('h1', {}, 'Adopt Me !'),
-  //     React.createElement(Pet, {
-  //       name: 'Nish',
-  //       animal: 'Dog',
-  //       breed: 'Havanese',
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: 'Scratch',
-  //       animal: 'Bird',
-  //       breed: 'Parrot',
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: 'Puss',
-  //       animal: 'Dog',
-  //       breed: 'mixed',
-  //     }),
-  //   ]);
+import SearchParams from './SearchParams';
 
+const App = () => {
   return (
     <div>
-      <h1>Adopt Me</h1>
-      <Pet name="Nish" animal="Dog" breed="Havanese" />
-      <Pet name="Scratch" animal="Bird" breed="Parrot" />
-      <Pet name="Puss" animal="Cat" breed="mixed" />
+      <h1 id="root">Adopt Me</h1>
+        <SearchParams/>
     </div>
   );
 };
-render(<App />, document.getElementById('root'));
+
+render(<App/>, document.getElementById('root'));
