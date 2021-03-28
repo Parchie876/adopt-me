91e79903a3e509b6d009fd8f0b3152164ecb6877
Dillon Parchment <dillonparchmentkool@gmail.com>
2021-03-27 21:00:38 -0500

    add npm scripts, eslint, Prettier, gitignore

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed)
-    ]);
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, props.name),
+    React.createElement('h2', {}, props.animal),
+    React.createElement('h2', {}, props.breed),
+  ]);
 };
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    [
-      React.createElement("h1", {}, "Adopt Me !"),
-      React.createElement(Pet, {
-        name: 'Nish',
-        animal: 'Dog',
-        breed: 'Havanese'
-      }),
-      React.createElement(Pet, {
-        name: 'Scratch',
-        animal: 'Bird',
-        breed: 'Parrot'
-      }),
-      React.createElement(Pet, {
-        name: 'Puss',
-        animal: 'Dog',
-        breed: 'mixed'
-      })
-
-    ]
-  );
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, 'Adopt Me !'),
+    React.createElement(Pet, {
+      name: 'Nish',
+      animal: 'Dog',
+      breed: 'Havanese',
+    }),
+    React.createElement(Pet, {
+      name: 'Scratch',
+      animal: 'Bird',
+      breed: 'Parrot',
+    }),
+    React.createElement(Pet, {
+      name: 'Puss',
+      animal: 'Dog',
+      breed: 'mixed',
+    }),
+  ]);
 };
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
