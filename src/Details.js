418530fe2efb0df723e4de4728b428ec3acf5bfb
Dillon Parchment <dillonparchmentkool@gmail.com>
2021-04-02 12:20:57 -0500

    Add error boundary to Details route

--- src/Details.js
+++ src/Details.js
@@ -2,10 +2,12 @@ import React from 'react';
 import pet from '@frontendmasters/pet';
 import Loader from './Loader';
 import Carousel from './Carousel';
+import ErrorBoundary from './ErrorBoundary';
 
 class Details extends React.Component {
   state = { loading: true };
   componentDidMount() {
+      throw new Error("lol")
     pet.animal(this.props.id).then(({ animal }) => {
       this.setState({
         name: animal.name,
@@ -55,4 +57,10 @@ class Details extends React.Component {
 //   );
 // };
 
-export default Details;
+export default function DetailsWithErrorBoundary(props) {
+    return (
+        <ErrorBoundary>
+            <Details {...props}/>
+        </ErrorBoundary>
+    )
+}
---
+++ src/ErrorBoundary.js
@@ -0,0 +1,41 @@
+// * See documentation @ reactjs.org/docs/error-boundaries.html
+
+import React, { Component } from 'react';
+import { Link, Redirect } from '@reach/router';
+import { Alert, AlertTitle } from '@material-ui/lab';
+import Loader from './Loader'
+
+class ErrorBoundary extends Component {
+  state = { hasError: false, redirect: false };
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+  componentDidCatch(error, info) {
+    console.error('ErrorBoundary caught an error', error, info);
+  }
+  componentDidUpdate() {
+    if (this.state.hasError) {
+      setTimeout(() => this.setState({ redirect: true }), 5000);
+    }
+  }
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    }
+    if (this.state.hasError) {
+      return (
+        <Alert severity="error">
+          <AlertTitle>Error</AlertTitle>
+          There was an error with this loading. —{' '}
+          <strong>
+            <Link to="/">Click here</Link> to go back to home or wait five
+            seconds <Loader />
+          </strong>
+        </Alert>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
